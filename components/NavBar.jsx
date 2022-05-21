0ab666d3a917ba4fbd78be4a72aa8474a0a3e43e
Abhisht Dixit <88162021+Abhisht01@users.noreply.github.com>
2022-05-21 16:21:39 +0530

    Adding components

---
+++ components/NavBar.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+import Link from "next/link";
+
+const NavBar = () => {
+  return (
+    <div className="w-full flex justify-between p-8">
+      <Link href="/" passHref>
+        <img
+          src="/static/images/logo_text_right_3.png"
+          alt=""
+          className="h-20 hover:cursor-pointer"
+        />
+      </Link>
+      <Link passHref href="/signup">
+        <div className="cta flex">
+          <img src="/static/images/user.svg" alt="" className="h-[25px] cta" />
+          <span className="hover-underline-animation">Sign Up / Login</span>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default NavBar;
